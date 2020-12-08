c084a33b65714c052e954665c8e7718720e1ae60
Aidan Sharp <enigma0fox@gmail.com>
2020-12-07 21:28:28 -0500

    small changes and a unity project

---
+++ Test/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class CharacterController : MonoBehaviour
+{
+    // Move player in 2D space
+    public float maxSpeed = 3.4f;
+    public float jumpHeight = 6.5f;
+    public Camera mainCamera;
+
+    bool facingRight = true;
+    float moveDirection = 0;
+    bool isGrounded = false;
+    Vector3 cameraPos;
+    Rigidbody r3d;
+    Collider mainCollider;
+    // Check every collider except Player and Ignore Raycast
+    LayerMask layerMask = ~(1 << 2 | 1 << 8);
+    Transform t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = transform;
+        r3d = GetComponent<Rigidbody>();
+        mainCollider = GetComponent<Collider>();
+        r3d.freezeRotation = true;
+        r3d.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        facingRight = t.localScale.x > 0;
+        gameObject.layer = 8;
+
+        if(mainCamera)
+            cameraPos = mainCamera.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement controls
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || r3d.velocity.x > 0.01f))
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || r3d.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+            }   
+        }
+
+        // Change facing direction
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+            }
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            r3d.velocity = new Vector3(r3d.velocity.x, jumpHeight);
+        }
+
+        // Camera follow
+        if(mainCamera)
+            mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+    }
+
+    void FixedUpdate()
+    {
+        Bounds colliderBounds = mainCollider.bounds;
+        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, 0.1f, 0);
+        // Check if player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos, 0.23f, layerMask);
+
+        // Apply movement velocity
+        r3d.velocity = new Vector3((moveDirection) * maxSpeed, r3d.velocity.y);
+
+        // Simple debug
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, 0.23f, 0), isGrounded ? Color.green : Color.red);
+    }
+}
